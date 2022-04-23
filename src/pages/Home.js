23aa5858f26c593fde64aee5c5ce58dd4f7eb6dd
Richard Schafer <richard.schafric@gmail.com>
2022-04-23 13:38:25 +0200

    add clearable input to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
+  Button,
   Text,
   VStack,
+  FormLabel,
+  Input,
+  Divider,
   Grid,
+  InputGroup,
+  InputLeftAddon,
 } from '@chakra-ui/react';
 
 function Home() {
+  const [inputValue, setInputValue] = useState("");
+
+  const clearInput = () => setInputValue("");
+  const handleChange = (event) => setInputValue(event.target.value);
+
   return (
     <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
@@ -15,6 +26,18 @@ function Home() {
                     Welcome to Bonus assignment home.<br />
                     Use navigation to navigate to add / subtract page, where you can play around with counter. 
                 </Text>
+                <Divider />
+
+                <FormLabel>Clearable input:</FormLabel>
+                <InputGroup>
+                  <InputLeftAddon children='Type & Clear 👉🏻' />
+                  <Input onChange={handleChange} value={inputValue} placeholder='... type something here' />
+                </InputGroup>
+
+                <Button color="green.50" bg="green.400" _hover={{bg: "green.600"}} onClick={clearInput}>
+                  Clear the input above
+                </Button>
+                
             </VStack>
         </Grid>
     </Box>
