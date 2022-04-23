390f5ff421f3d80ce5393b7716e9c70acce6f446
Richard Schafer <richard.schafric@gmail.com>
2022-04-23 12:09:56 +0200

    initial commit with cleaned create-react-app defaults

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Text>
+              <Code fontSize="xl">simple-react-app</Code> bonus assignment.
+            </Text>
+          </VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
